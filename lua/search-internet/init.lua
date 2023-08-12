c2a1fe233429717ba3d8590579292d63b8309e9c
Andres Lowrie <andres.lowrie@gmail.com>
2023-08-11 20:50:35 -0500

    yay... firing off a cmd works

--- lua/search-internet/init.lua
+++ lua/search-internet/init.lua
@@ -1,5 +1,6 @@
 -- Figure out which os I'm in
 -- call command to open the browser with some path
+local P = require('for_deving').P
 
 local os_to_cmd = {
   darwin = "open",
@@ -15,16 +16,26 @@ local determined_os = "darwin"
 
 local browse = function(url)
   local cmd = os_to_cmd[determined_os]
-  vim.fn.jobstart({ cmd, url })
+  -- local cmd = 'echo'
+  vim.fn.jobstart({ cmd, url }, {
+    on_stdout = function(j, d, e)
+      P('xhere')
+      P(d)
+      P(e)
+    end
+  })
 end
 
 
 -- @TODO:
---  1 come up with order of precedence perhaps something like:
---    visual selected
---    word under curosr
---    clipboard
---    ...
+--  1 ~come up with order of precedence perhaps something like:~
+--    instead of order of precedence, the function should take a search term
+--    and then do the search.
+--    the settinsgs should figure out what the term is
+--      visual selected
+--      word under curosr
+--      clipboard
+--      etc
 --  2 create url encoded string from query stuff
 --    each engine would have to follow some interface or something
 --  3 pass that url into the `browse` function
